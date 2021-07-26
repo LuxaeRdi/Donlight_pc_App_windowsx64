3049b66d988debf2ef6aba1767444c07dca863d6
javierMLuxae <javier.munoz@luxae.com>
2021-07-26 10:21:26 +0200

    added direct access, changed wokspace path of QT to the build folder not where the executable is

---
+++ rooftop 6/build-LuxaeGuiQml-Desktop_Qt_5_15_2_MinGW_64_bit-Release/release/moc_blecoms.cpp
@@ -0,0 +1,320 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'blecoms.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../LuxaeGuiQml/blecoms.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'blecoms.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_blecoms_t {
+    QByteArrayData data[40];
+    char stringdata0[720];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_blecoms_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_blecoms_t qt_meta_stringdata_blecoms = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "blecoms"
+QT_MOC_LITERAL(1, 8, 13), // "blediscovered"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 4), // "data"
+QT_MOC_LITERAL(4, 28, 13), // "alreadyopened"
+QT_MOC_LITERAL(5, 42, 27), // "deviceDiscoveryCacelledSLOT"
+QT_MOC_LITERAL(6, 70, 20), // "deviceDiscoveredSLOT"
+QT_MOC_LITERAL(7, 91, 20), // "QBluetoothDeviceInfo"
+QT_MOC_LITERAL(8, 112, 6), // "device"
+QT_MOC_LITERAL(9, 119, 17), // "deviceUpdatedSLOT"
+QT_MOC_LITERAL(10, 137, 28), // "QBluetoothDeviceInfo::Fields"
+QT_MOC_LITERAL(11, 166, 13), // "updatedFields"
+QT_MOC_LITERAL(12, 180, 18), // "discoveryerrorSLOT"
+QT_MOC_LITERAL(13, 199, 37), // "QBluetoothDeviceDiscoveryAgen..."
+QT_MOC_LITERAL(14, 237, 5), // "error"
+QT_MOC_LITERAL(15, 243, 21), // "discoveryfinishedSLOT"
+QT_MOC_LITERAL(16, 265, 10), // "connectBLE"
+QT_MOC_LITERAL(17, 276, 13), // "disconnectBLE"
+QT_MOC_LITERAL(18, 290, 15), // "bleStatechanged"
+QT_MOC_LITERAL(19, 306, 37), // "QLowEnergyController::Control..."
+QT_MOC_LITERAL(20, 344, 5), // "state"
+QT_MOC_LITERAL(21, 350, 12), // "bleConnected"
+QT_MOC_LITERAL(22, 363, 21), // "bleServicesDiscovered"
+QT_MOC_LITERAL(23, 385, 29), // "bleCharacteristicsChangedSLOT"
+QT_MOC_LITERAL(24, 415, 24), // "QLowEnergyCharacteristic"
+QT_MOC_LITERAL(25, 440, 14), // "characteristic"
+QT_MOC_LITERAL(26, 455, 8), // "newValue"
+QT_MOC_LITERAL(27, 464, 26), // "bleCharacteristicsReadSLOT"
+QT_MOC_LITERAL(28, 491, 28), // "bleCharacteristicWrittenSLOT"
+QT_MOC_LITERAL(29, 520, 21), // "bleDescriptorReadSLOT"
+QT_MOC_LITERAL(30, 542, 20), // "QLowEnergyDescriptor"
+QT_MOC_LITERAL(31, 563, 10), // "descriptor"
+QT_MOC_LITERAL(32, 574, 5), // "value"
+QT_MOC_LITERAL(33, 580, 24), // "bleDescriptorWrittenSLOT"
+QT_MOC_LITERAL(34, 605, 12), // "bleErrorSLOT"
+QT_MOC_LITERAL(35, 618, 31), // "QLowEnergyService::ServiceError"
+QT_MOC_LITERAL(36, 650, 8), // "newError"
+QT_MOC_LITERAL(37, 659, 19), // "bleStateChangedSLOT"
+QT_MOC_LITERAL(38, 679, 31), // "QLowEnergyService::ServiceState"
+QT_MOC_LITERAL(39, 711, 8) // "newState"
+
+    },
+    "blecoms\0blediscovered\0\0data\0alreadyopened\0"
+    "deviceDiscoveryCacelledSLOT\0"
+    "deviceDiscoveredSLOT\0QBluetoothDeviceInfo\0"
+    "device\0deviceUpdatedSLOT\0"
+    "QBluetoothDeviceInfo::Fields\0updatedFields\0"
+    "discoveryerrorSLOT\0"
+    "QBluetoothDeviceDiscoveryAgent::Error\0"
+    "error\0discoveryfinishedSLOT\0connectBLE\0"
+    "disconnectBLE\0bleStatechanged\0"
+    "QLowEnergyController::ControllerState\0"
+    "state\0bleConnected\0bleServicesDiscovered\0"
+    "bleCharacteristicsChangedSLOT\0"
+    "QLowEnergyCharacteristic\0characteristic\0"
+    "newValue\0bleCharacteristicsReadSLOT\0"
+    "bleCharacteristicWrittenSLOT\0"
+    "bleDescriptorReadSLOT\0QLowEnergyDescriptor\0"
+    "descriptor\0value\0bleDescriptorWrittenSLOT\0"
+    "bleErrorSLOT\0QLowEnergyService::ServiceError\0"
+    "newError\0bleStateChangedSLOT\0"
+    "QLowEnergyService::ServiceState\0"
+    "newState"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_blecoms[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    2,  104,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       5,    0,  109,    2, 0x0a /* Public */,
+       6,    1,  110,    2, 0x0a /* Public */,
+       9,    2,  113,    2, 0x0a /* Public */,
+      12,    1,  118,    2, 0x0a /* Public */,
+      15,    0,  121,    2, 0x0a /* Public */,
+      16,    1,  122,    2, 0x0a /* Public */,
+      17,    0,  125,    2, 0x0a /* Public */,
+      18,    1,  126,    2, 0x0a /* Public */,
+      21,    0,  129,    2, 0x0a /* Public */,
+      22,    0,  130,    2, 0x0a /* Public */,
+      23,    2,  131,    2, 0x0a /* Public */,
+      27,    2,  136,    2, 0x0a /* Public */,
+      28,    2,  141,    2, 0x0a /* Public */,
+      29,    2,  146,    2, 0x0a /* Public */,
+      33,    2,  151,    2, 0x0a /* Public */,
+      34,    1,  156,    2, 0x0a /* Public */,
+      37,    1,  159,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QVariant, QMetaType::Int,    3,    4,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void, 0x80000000 | 7, 0x80000000 | 10,    8,   11,
+    QMetaType::Void, 0x80000000 | 13,   14,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 19,   20,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 24, QMetaType::QByteArray,   25,   26,
+    QMetaType::Void, 0x80000000 | 24, QMetaType::QByteArray,   25,   26,
+    QMetaType::Void, 0x80000000 | 24, QMetaType::QByteArray,   25,   26,
+    QMetaType::Void, 0x80000000 | 30, QMetaType::QByteArray,   31,   32,
+    QMetaType::Void, 0x80000000 | 30, QMetaType::QByteArray,   31,   26,
+    QMetaType::Void, 0x80000000 | 35,   36,
+    QMetaType::Void, 0x80000000 | 38,   39,
+
+       0        // eod
+};
+
+void blecoms::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<blecoms *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->blediscovered((*reinterpret_cast< QVariant(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 1: _t->deviceDiscoveryCacelledSLOT(); break;
+        case 2: _t->deviceDiscoveredSLOT((*reinterpret_cast< const QBluetoothDeviceInfo(*)>(_a[1]))); break;
+        case 3: _t->deviceUpdatedSLOT((*reinterpret_cast< const QBluetoothDeviceInfo(*)>(_a[1])),(*reinterpret_cast< QBluetoothDeviceInfo::Fields(*)>(_a[2]))); break;
+        case 4: _t->discoveryerrorSLOT((*reinterpret_cast< const QBluetoothDeviceDiscoveryAgent::Error(*)>(_a[1]))); break;
+        case 5: _t->discoveryfinishedSLOT(); break;
+        case 6: _t->connectBLE((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 7: _t->disconnectBLE(); break;
+        case 8: _t->bleStatechanged((*reinterpret_cast< QLowEnergyController::ControllerState(*)>(_a[1]))); break;
+        case 9: _t->bleConnected(); break;
+        case 10: _t->bleServicesDiscovered(); break;
+        case 11: _t->bleCharacteristicsChangedSLOT((*reinterpret_cast< const QLowEnergyCharacteristic(*)>(_a[1])),(*reinterpret_cast< const QByteArray(*)>(_a[2]))); break;
+        case 12: _t->bleCharacteristicsReadSLOT((*reinterpret_cast< const QLowEnergyCharacteristic(*)>(_a[1])),(*reinterpret_cast< const QByteArray(*)>(_a[2]))); break;
+        case 13: _t->bleCharacteristicWrittenSLOT((*reinterpret_cast< const QLowEnergyCharacteristic(*)>(_a[1])),(*reinterpret_cast< const QByteArray(*)>(_a[2]))); break;
+        case 14: _t->bleDescriptorReadSLOT((*reinterpret_cast< const QLowEnergyDescriptor(*)>(_a[1])),(*reinterpret_cast< const QByteArray(*)>(_a[2]))); break;
+        case 15: _t->bleDescriptorWrittenSLOT((*reinterpret_cast< const QLowEnergyDescriptor(*)>(_a[1])),(*reinterpret_cast< const QByteArray(*)>(_a[2]))); break;
+        case 16: _t->bleErrorSLOT((*reinterpret_cast< QLowEnergyService::ServiceError(*)>(_a[1]))); break;
+        case 17: _t->bleStateChangedSLOT((*reinterpret_cast< QLowEnergyService::ServiceState(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 2:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QBluetoothDeviceInfo >(); break;
+            }
+            break;
+        case 3:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QBluetoothDeviceInfo >(); break;
+            }
+            break;
+        case 8:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyController::ControllerState >(); break;
+            }
+            break;
+        case 11:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyCharacteristic >(); break;
+            }
+            break;
+        case 12:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyCharacteristic >(); break;
+            }
+            break;
+        case 13:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyCharacteristic >(); break;
+            }
+            break;
+        case 14:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyDescriptor >(); break;
+            }
+            break;
+        case 15:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyDescriptor >(); break;
+            }
+            break;
+        case 16:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyService::ServiceError >(); break;
+            }
+            break;
+        case 17:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QLowEnergyService::ServiceState >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (blecoms::*)(QVariant , int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&blecoms::blediscovered)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject blecoms::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_blecoms.data,
+    qt_meta_data_blecoms,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *blecoms::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *blecoms::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_blecoms.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int blecoms::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void blecoms::blediscovered(QVariant _t1, int _t2)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
