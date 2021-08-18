4ab074de306e17b47be85dd1c8503e3753ca2df7
javierMLuxae <javier.munoz@luxae.com>
2021-08-18 12:33:39 +0200

    updated binaries with polaris firmware, data is 1 byte shifted

--- LuxaeGuiQml_shortcut.bat
+++ LuxaeGuiQml_shortcut.bat
@@ -1,2 +1,8 @@
+::===============================================================
+:: Goes to where the .exe is located inside the build folder and executes it.
+::
+::===============================================================
 CD ".\build-LuxaeGuiQml-Desktop_Qt_5_15_2_MinGW_64_bit-Release\release"
+
 @start "" "LuxaeGuiQml.exe"
+Title “LuxaeGuiQml shortcut”
